ef4c268ed929e0300c55f8125260d85a61d6209c
Flor Jara <florenciajara96@gmail.com>
2022-05-26 12:05:27 -0300

    primer commit

---
+++ src/test/java/edu/egg/inmobiliaria/InmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.egg.inmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
