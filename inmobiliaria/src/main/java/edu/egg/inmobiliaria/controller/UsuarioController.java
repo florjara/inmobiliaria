1523792bb4b0e3497367c4684779faf1a607cb8b
Elias Nicolas Medina Villalba <eliasnikolasmv@gmail.com>
2022-06-06 20:21:51 -0300

    Add files via upload
    Error

---
+++ inmobiliaria/src/main/java/edu/egg/inmobiliaria/controller/UsuarioController.java
@@ -0,0 +1,110 @@
+package edu.egg.inmobiliaria.controller;
+
+import edu.egg.inmobiliaria.entity.Usuario;
+import edu.egg.inmobiliaria.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController{
+
+    private final UsuarioService usuarioService;
+
+    @Autowired
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+
+    @GetMapping("/login")
+    public ModelAndView login(@RequestParam(required = false) String error, @RequestParam(required = false) String logout, Principal principal) {
+        ModelAndView mav = new ModelAndView("login_usuario");
+
+        if (error != null) mav.addObject("error", "Correo o contraseña erroneos");
+        if (logout != null) mav.addObject("logout", "Saliste satisfactoriamente de la sesion");
+        if (principal != null) mav.setViewName("redirect:/");
+
+
+        return mav;
+    }
+
+    @GetMapping
+    public ModelAndView getUsuarios(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView("index");
+        Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
+
+        if (inputFlashMap != null) mav.addObject("success", inputFlashMap.get("success"));
+
+        mav.addObject("usuarios", usuarioService.getAll());
+        return mav;
+    }
+
+    @GetMapping("/sign-up")
+    public ModelAndView signup(HttpServletRequest solicitud, Principal principal) {
+        ModelAndView mav = new ModelAndView("form_usuario");
+        Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(solicitud);
+
+        if (principal != null) mav.setViewName("redirect:/");
+
+        if (inputFlashMap != null) {
+            mav.addObject("excepcion", inputFlashMap.get("excepcion"));
+            mav.addObject("usuario", inputFlashMap.get("usuario"));
+        } else {
+            mav.addObject("usuario", new Usuario());
+            mav.addObject("action", "registrar");
+        }
+        return mav;
+    }
+
+    @GetMapping("/form/{id}")
+    public ModelAndView getForm(@PathVariable Long id) {
+        ModelAndView mav = new ModelAndView("form_usuario");
+        mav.addObject("usuario", usuarioService.getById(id));
+        mav.addObject("action", "actualizar");
+        return mav;
+    }
+
+
+    @PostMapping("/registrar")
+    public RedirectView signup(Usuario usuarioDto, RedirectAttributes atributos) {
+        RedirectView redireccion = new RedirectView("/");
+
+
+        try {
+            usuarioService.create(usuarioDto);
+        } catch (IllegalArgumentException e) {
+            atributos.addFlashAttribute("usuario", usuarioDto);
+            atributos.addFlashAttribute("excepcion", e.getMessage());
+            redireccion.setUrl("/usuarios/sign-up");
+        }
+
+        return redireccion;
+    }
+
+    @PostMapping("/actualizar")
+    public RedirectView actualizar(Usuario usuarioDto, RedirectAttributes attributes) {
+        RedirectView redirect = new RedirectView("/usuarios");
+        usuarioService.update(usuarioDto);
+        attributes.addFlashAttribute("success", "The operation has been carried out successfully");
+        return redirect;
+    }
+
+
+    @PostMapping("/eliminar/{id}")
+    public RedirectView eliminar(@PathVariable Long id) {
+        RedirectView redirect = new RedirectView("/usuarios");
+        usuarioService.deleteById(id);
+        return redirect;
+    }
+
+}
---
+++ inmobiliaria/src/main/java/edu/egg/inmobiliaria/service/UsuarioService.java
@@ -0,0 +1,98 @@
+package edu.egg.inmobiliaria.service;
+
+import edu.egg.inmobiliaria.entity.Usuario;
+import edu.egg.inmobiliaria.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+import static java.util.Collections.emptyList;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+
+
+    private final UsuarioRepository usuarioRepository;
+    private final BCryptPasswordEncoder encriptador;
+
+    @Autowired
+    public UsuarioService(UsuarioRepository usuarioRepository, BCryptPasswordEncoder encriptador) {
+        this.usuarioRepository = usuarioRepository;
+        this.encriptador = encriptador;
+    }
+
+
+    //crea un usuario
+    @Transactional
+    public void create (Usuario dto){
+
+        if (usuarioRepository.existsByCorreo(dto.getCorreo()))
+            throw new IllegalArgumentException("Este correo ya fue registrado con aterioridad");
+
+        Usuario usuario = new Usuario();
+
+        usuario.setNombre(dto.getNombre());
+        usuario.setApellido(dto.getApellido());
+        usuario.setContrasena(encriptador.encode(dto.getContrasena()));
+        usuario.setCorreo(dto.getCorreo());
+        usuario.setTelefono(dto.getTelefono());
+
+        usuarioRepository.save(usuario);
+    }
+
+    //actualiza un usuario
+    @Transactional
+    public void update (Usuario dto){
+
+        Usuario usuario = usuarioRepository.findById(dto.getId()).get();
+
+        usuario.setNombre(dto.getNombre());
+        usuario.setApellido(dto.getApellido());
+        usuario.setContrasena(encriptador.encode(dto.getContrasena()));
+        usuario.setCorreo(dto.getCorreo());
+        usuario.setTelefono(dto.getTelefono());
+
+        usuarioRepository.save(usuario);
+
+    }
+
+    //busca un usuario por id
+    @Transactional(readOnly = true)
+    public Usuario getById(Long id){
+        return usuarioRepository.findById(id).get();
+    }
+
+    //devuelve la lista de todos los usuarios
+    @Transactional(readOnly = true)
+    public List<Usuario> getAll(){
+        return usuarioRepository.findAll();
+    }
+
+    //habilita un usuario
+    @Transactional
+    public void enableById(Long id){
+        usuarioRepository.getReferenceById(id);
+    }
+
+    //borra un usuario por id
+    @Transactional
+    public void deleteById(Long id){
+        usuarioRepository.deleteById(id);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String correo) throws UsernameNotFoundException{
+        Supplier<UsernameNotFoundException> supplier = () -> new UsernameNotFoundException("No existe un usuario registrado con este correo");
+        Usuario usuario = usuarioRepository.findByCorreo(correo).orElseThrow(supplier);
+
+        return new User(usuario.getCorreo(), usuario.getContrasena(), emptyList());
+    }
+}
