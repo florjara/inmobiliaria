a55acbdbaa92d77a87af43183594cdf18d738bae
Elias Nicolas Medina Villalba <eliasnikolasmv@gmail.com>
2022-06-06 20:23:47 -0300

    Delete inmobiliaria directory

--- inmobiliaria/src/main/java/edu/egg/inmobiliaria/repository/PropiedadRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.egg.inmobiliaria.repository;
-
-import edu.egg.inmobiliaria.entity.Propiedad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropiedadRepository extends JpaRepository<Propiedad, Long> {
-}
