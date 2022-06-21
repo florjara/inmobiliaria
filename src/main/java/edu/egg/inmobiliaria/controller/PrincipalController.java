3b722ceaf5fb1fe451769404a7b69821253f3fae
Brainton <hbraiton@gmail.com>
2022-06-21 12:42:18 -0300

    imagen de hombre pensando y centre los header

--- src/main/java/edu/egg/inmobiliaria/controller/PrincipalController.java
+++ src/main/java/edu/egg/inmobiliaria/controller/PrincipalController.java
@@ -1,14 +1,18 @@
 package edu.egg.inmobiliaria.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 @Controller
 public class PrincipalController {
 
     @GetMapping
     public ModelAndView getIndex() {
-        return new ModelAndView("index");
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
     }
 }
