f9e94ecba9ed4bf048fb14b58b566bcc5766dd73
Alfredo0688 <alfredonunez0688@gmail.com>
2022-06-21 20:57:29 -0300

    Scripts actualizados

--- scriptNuevo.sql
+++ scriptNuevo.sql
@@ -1,3 +1,12 @@
+use inmobiliaria;
+INSERT INTO rol (rol_id, eliminado, rol_nombre) VALUES (NULL, 0, "ADMIN"),
+(NULL, 0, "USUARIO");
+
+INSERT INTO images(propiedad_id,imagen)VALUES(1,'casa1a.jpg'),(1,'casa1b.jpg'),(1,'casa1c.jpg'),(2,'casa2a.jpg'),(2,'casa2b.jpg'),(2,'casa2c.jpg'),
+(3,'casa3a.jpg'),(3,'casa3b.jpg'),(3,'casa3b.jpg'),(4,'casa4a.jpg'),(4,'casa4b.jpg'),(4,'casa4c.jpg'),(5,'casa5a.jpg'),(5,'casa5b.jpg'),
+(5,'casa5c.jpg'),(6,'casa6a.jpg'),(6,'casa6b.jpg'),(6,'casa6c.jpg'),(7,'casa7a.jpg'),(7,'casa7b.jpg'),(7,'casa7c.jpg'),(8,'casa8a.jpg'),(8,'casa8b.jpg'),
+(8,'casa8c.jpg');
+
 
 INSERT INTO propiedad(id,ambiente,banos,ciudad,descripcion,direccion,eliminado,estacionamiento,patio,precio,tipo,tipo_transaccion,titulo,usuario) VALUES 
 
@@ -13,10 +22,10 @@ Terreno de 505m2 !!","La Paz 500",0,1,1,9797775,"CASA","VENTA","Venta Casa con P
 ",0,1,1,70000,"CASA","ALQUILER","Casa de 2 Dorm. Luchessi E/ Lavalle y Fco. de Haro",2),
 
 (5,7,1,"OBERA", "Casa amplia en barrio muy tranquilo, 3 dormitorios + 1 local, cocina, comedor, living, cochera techada para 4 autos, quincho, pileta, muros perimetrales, sobre calle asfaltada con buena iluminación",
-"Olavarria 1254",0,1,1,9225089,"CASA","VENTA","Amplia Casa",3),
+"Olavarria 1254",0,1,1,9225089,"CASA","VENTA","Amplia Casa",2),
 
-(6,3,1,"POSADAS", "Departamento a pasos de la costanera, ubicados a una cuadra de la playa 'El Brete', sobre calle Mbororé Nº2635 casi calle Iguazú. a pasos del centro de Posadas.
-El mismo cuenta con dos habitaciones, cocina con alacena y bajo mesada, sector de lavadero, amplio baño, aire acondicionado, placard.","Avenida Quaranta 235",0,1,0,70000,"DEPARTAMENTO","ALQUILER","Departamento de Dos Dorm. en Zona El Brete",3),
+(6,3,1,"POSADAS", "Departamento sobre calle Mbororé Nº2635 casi calle Iguazú. a pasos del centro de Posadas.
+Cuenta con dos habitaciones, cocina con alacena y bajo mesada, sector de lavadero, amplio baño, aire acondicionado, placard.","Avenida Quaranta 235",0,1,0,70000,"DEPARTAMENTO","ALQUILER","Departamento de Dos Dorm. en Zona El Brete",2),
 
 (7,4,2,"POSADAS", "HERMOSA PROPIEDAD EN VENTA EN BARRIO SAN MARTIN.","Padre Serrano 2909. Entre Antonio de llamas y Av blas parera",0,1,1,85000,"CASA","ALQUILER","Casa de 3 Dorm. en Venta",2),
 
