1030953c5dcce995d724aec01f4d2d1733e44810
Alfredo0688 <alfredonunez0688@gmail.com>
2022-06-21 20:29:51 -0300

    Script propiedades agregado

---
+++ scriptPropiedad.sql
@@ -0,0 +1,25 @@
+
+INSERT INTO propiedad(id,ambiente,banos,ciudad,descripcion,direccion,eliminado,estacionamiento,patio,precio,tipo,tipo_transaccion,titulo,usuario) VALUES 
+
+(1,2,1,"POSADAS", "flexible, que se adecua a tus necesidades de hoy y mañana.","Avenida Quaranta 235",0,0,0,4500000,"DEPARTAMENTO","VENTA","Departamento dos ambientes",1),
+
+(2,3,1,"POSADAS", "Alquiler de Casa 3 DORMITORIOS en Las Orquideas","Av Urquiza 4600. Entre Las araucarias y Av las heras
+",0,1,1,100000,"CASA","ALQUILER","Casa en Barrio Las Orquideas",2),
+
+(3,6,3,"OBERA", "Preciosa casa desarrollada en dos plantas, con piscina en un entorno inmejorable.
+Terreno de 505m2 !!","La Paz 500",0,1,1,9797775,"CASA","VENTA","Venta Casa con Pileta Oberá Misiones",2),
+
+(4,3,1,"POSADAS", "Consta de dos dormitorios (uno con aire acondicionado), baño, Cocina-comedor, galerias al frente y al costado, y cochera","Luchessi 2748. Entre Gomez portinho y Sebastopol
+",0,1,1,70000,"CASA","ALQUILER","Casa de 2 Dorm. Luchessi E/ Lavalle y Fco. de Haro",2),
+
+(5,7,1,"OBERA", "Casa amplia en barrio muy tranquilo, 3 dormitorios + 1 local, cocina, comedor, living, cochera techada para 4 autos, quincho, pileta, muros perimetrales, sobre calle asfaltada con buena iluminación",
+"Olavarria 1254",0,1,1,9225089,"CASA","VENTA","Amplia Casa",3),
+
+(6,3,1,"POSADAS", "Departamento a pasos de la costanera, ubicados a una cuadra de la playa 'El Brete', sobre calle Mbororé Nº2635 casi calle Iguazú. a pasos del centro de Posadas.
+El mismo cuenta con dos habitaciones, cocina con alacena y bajo mesada, sector de lavadero, amplio baño, aire acondicionado, placard.","Avenida Quaranta 235",0,1,0,70000,"DEPARTAMENTO","ALQUILER","Departamento de Dos Dorm. en Zona El Brete",3),
+
+(7,4,2,"POSADAS", "HERMOSA PROPIEDAD EN VENTA EN BARRIO SAN MARTIN.","Padre Serrano 2909. Entre Antonio de llamas y Av blas parera",0,1,1,85000,"CASA","ALQUILER","Casa de 3 Dorm. en Venta",2),
+
+(8,7,2,"OBERA", "","Chubut 100",0,1,1,1000000,"CASA","VENTA","En Venta, Casa Céntrica En Oberá, Diseño Y Calidad",1),
+
+(9,2,1,"POSADAS", "flexible, que se adecua a tus necesidades de hoy y mañana.","Avenida Quaranta 235",0,0,0,7500000,"DEPARTAMENTO","VENTA","Departamento dos ambientes",1);
